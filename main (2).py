6b9e9143d1f969df01423260106e9f8e1c9891ab
aryaputra1237y-svg <aryaputra1237y@gmail.com>
2026-04-09 07:04:29 +0700

    Delete main (2).py

--- main (2).py
+++
@@ -1,2 +0,0 @@
-print("Hello ganteng");print("perkenalkan nama saya inces"); print("nama saya arya");print("ouh yaa");print("mau apa kak");
-print("mau nanya");print("ini dimna"); print ("ini di soljer ");
